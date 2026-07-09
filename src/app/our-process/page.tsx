1aeb57861aa62f4caebe5d21b4172dbfa838d79a
Nandini <nandusankhe@gmail.com>
2026-07-09 20:13:02 +0530

    feat:our-processes page

---
+++ src/app/our-process/page.tsx
@@ -0,0 +1,12 @@
+import PageBanner from "@/src/common/components/layouts/PageBanner";
+import ProcessSection from "../(home)/_componts/ProcessSection";
+
+export default function Page() {
+    return (
+      <>
+            <PageBanner title="Our Process" />
+                  <ProcessSection />
+            
+</>
+  );
+}
