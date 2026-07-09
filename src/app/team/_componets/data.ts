943f178ded8b61006b6aea5bcbd4309240550675
Nandini <nandusankhe@gmail.com>
2026-07-09 20:11:20 +0530

    feat:teams data

---
+++ src/app/team/_componets/data.ts
@@ -0,0 +1,49 @@
+export const founder = {
+    name: "MR NAGENDRA MISHRA",
+    role: "FOUNDER",
+    image: "/assets/jpg/our_team_nagendra_sir_profile.jpg",
+    description:
+      "Nagendra founded Visualytes in 2015. Quality and user friendly website development is a valuable investment for your business. Get your business online to give your business space to grow.",
+  };
+  
+  export const directors = [
+    {
+      image: "/assets/jpg/team-directors-400x400.jpg",
+      role: "COMPANY ROLE : MANAGING DIRECTOR",
+    },
+  ];
+  
+  export const team = [
+    {
+      image: "/assets/jpg/sales-ot-400x400.jpg",
+      role: "COMPANY ROLE : ACCOUNT MANAGER",
+    },
+    {
+      image: "/assets/jpg/sales-ot-400x400.jpg",
+      role: "COMPANY ROLE : ACCOUNT MANAGER",
+    },
+    {
+      image: "/assets/jpg/team-developers-400x400.jpg",
+      role: "COMPANY ROLE : DEVELOPER",
+    },
+    {
+      image: "/assets/jpg/team-developers-400x400.jpg",
+      role: "COMPANY ROLE : DEVELOPER",
+    },
+    {
+      image: "/assets/jpg/team-testers-400x400.jpg",
+      role: "COMPANY ROLE : TESTER",
+    },
+    {
+      image: "/assets/jpg/team-testers-400x400.jpg",
+      role: "COMPANY ROLE : TESTER",
+    },
+    {
+      image: "/assets/jpg/team-marketors-400x400.jpg",
+      role: "COMPANY ROLE : MARKETERS",
+    },
+    {
+      image: "/assets/jpg/team-marketors-400x400.jpg",
+      role: "COMPANY ROLE : MARKETERS",
+    },
+  ];
